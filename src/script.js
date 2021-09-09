fc9cd0e1f2fb1c27ad46721759b7516ac5e91129
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 20:07:00 -0300

    ✨ feat: script use ui classes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import { DarkModeUI } from "./classes/dark-mode-ui.js";
+import { SinglePageApplication } from "./classes/single-page-application.js";
 
 export const $main = document.querySelector("#root");
 export const $header = document.querySelector("header");
 
 DarkModeUI.headerListener();
+SinglePageApplication.addHashListener();
+SinglePageApplication.addWindowLoadListener();
