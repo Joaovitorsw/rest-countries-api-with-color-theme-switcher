a9c02eb609fbba82068483ffbee4a5b032659cda
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-08 23:34:39 -0300

    ✨ feat: create div homepage and navigation-content

--- src/script.js
+++ src/script.js
@@ -2,3 +2,5 @@ import { DarkModeUI } from "./classes/dark-mode-ui.js";
 
 export const $main = document.querySelector("#root");
 export const $header = document.querySelector("header");
+
+DarkModeUI.headerListener();
