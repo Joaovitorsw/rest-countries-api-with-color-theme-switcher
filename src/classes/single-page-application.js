273a7684ed9f9bc9dc39c5caa9cdb920d9afe37c
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 19:06:30 -0300

    ✨ feat: spa render page

--- src/classes/single-page-application.js
+++ src/classes/single-page-application.js
@@ -9,6 +9,21 @@ export class SinglePageApplication {
     const hashIsEmpty = hash === "";
     return hashIsEmpty ? "home" : hash.replace("#", "");
   }
+  static async renderPage() {
+    const hashedRoute = window.location.hash;
+    const targetRoute = SinglePageApplication.getTargetRoute(hashedRoute);
+
+    const [fragment, param] = targetRoute.split("/");
+    const renderPageFn = ROUTES[fragment];
+
+    const hasParam = !!param;
+    const html = hasParam ? await renderPageFn(param) : await renderPageFn();
+
+    $main.innerHTML = "";
+    $main.appendChild(html);
+
+    if (hashedRoute === "") return SinglePageApplication.setupHome();
+  }
   static addWindowLoadListener() {
     window.addEventListener("load", async () => {
       await SinglePageApplication.renderPage();
