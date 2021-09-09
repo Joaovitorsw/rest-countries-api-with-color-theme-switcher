1aafef77ed2f85bd03b47dfa1de66411f9d1f7f4
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 19:03:34 -0300

    ✨ feat: class routes

---
+++ src/classes/routes.js
@@ -0,0 +1,7 @@
+import { CountryPage } from "../pages/country-page.js";
+import { HomePage } from "../pages/homepage.js";
+
+export const ROUTES = {
+  home: HomePage,
+  countrypage: CountryPage,
+};
