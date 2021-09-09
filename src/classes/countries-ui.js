fd44de84168f497a658be80a2057e785cbf61340
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 20:47:58 -0300

    ✨ feat: countries ui create class and method create country card

---
+++ src/classes/countries-ui.js
@@ -0,0 +1,30 @@
+import { CountriesAPI } from "../classes/countries-api.js";
+
+export class CountriesUI {
+  createCountryCard(country) {
+    const $country = document.createElement("div");
+    $country.classList.add("country");
+    $country.id = country.alpha2Code;
+
+    const populationToDecimal = country.population.toLocaleString("pt-BR");
+
+    const countryInnerHTML = ` <a href="/#countrypage/${$country.id}"><img
+            src="${country.flag}"
+            alt="${country.alpha3Code}"
+            class="country-flag"
+          />
+          <h1 class="country-name">${country.name}</h1>
+          <h2 class="country-population">
+            Population:<span class="population-value">${populationToDecimal}</span>
+          </h2>
+          <h3 class="country-region">
+            Region:<span class="region-value">${country.region}</span>
+          </h3>
+          <h4 class="country-capital">
+            Capital:<span class="capital-value">${country.capital}</span>
+          </h4><a/>`;
+
+    $country.innerHTML = countryInnerHTML;
+    this.$countriesContent.appendChild($country);
+  }
+}
