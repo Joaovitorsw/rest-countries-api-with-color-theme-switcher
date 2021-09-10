e875031152991cd5bb855dba00a0413acae80284
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 21:06:50 -0300

    ✨ feat:  dynamic borders create borders countries

--- src/classes/dynamic-borders.js
+++ src/classes/dynamic-borders.js
@@ -3,4 +3,32 @@ export class DynamicBorders {
   constructor(countriesBorders) {
     this.#countriesBorders = countriesBorders;
   }
+  dynamicBordersUI() {
+    return this.createContent();
+  }
+  async createContent() {
+    const $divBorders = document.createElement("div");
+    const title = document.createElement("h2");
+    const $countryPageBorders = await this.createBordersCountries();
+
+    $divBorders.classList.add("country-page-shortcuts");
+    title.innerText = "Border Countries:";
+    $divBorders.appendChild(title);
+    $divBorders.appendChild($countryPageBorders);
+
+    return $divBorders;
+  }
+  async createBordersCountries() {
+    const $countryPageBorders = document.createElement("ul");
+    const countryBorders = this.#countriesBorders;
+
+    $countryPageBorders.classList.add("country-page-borders");
+
+    countryBorders.forEach(async (country) => {
+      const $countryPage = await this.borderCountry(country);
+      $countryPageBorders.append($countryPage);
+    });
+
+    return $countryPageBorders;
+  }
 }
