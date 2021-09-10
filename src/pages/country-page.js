09b593113f7f59bc63b3e4ca3fd71d2af44dcf65
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-09 21:00:57 -0300

    ✨ feat: country page elements create and add classe

--- src/pages/country-page.js
+++ src/pages/country-page.js
@@ -1,4 +1,12 @@
 export async function CountryPage(id) {
   const request = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
   const actuallyCountry = await request.json();
+
+  const $countryPage = document.createElement("div");
+  const $countryContent = document.createElement("div");
+  const $countryText = document.createElement("div");
+
+  $countryPage.classList.add("country-page");
+  $countryContent.classList.add("country-content");
+  $countryText.classList.add("country-text");
 }
