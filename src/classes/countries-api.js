a17036c782ccdd09d2a92abe5282bd4caf8b9159
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-23 21:32:48 -0300

    📦 refactor: api url update

--- src/classes/countries-api.js
+++ src/classes/countries-api.js
@@ -1,13 +1,12 @@
 export class CountriesAPI {
   static getCountries = async () => {
-    const countries = await fetch(`https://restcountries.eu/rest/v2/all
+    const countries = await fetch(`https://restcountries.eu/rest/v3/all
     `);
     return countries.json();
   };
 
   static getRegions = async (region) => {
-    const regionRequest =
-      await fetch(`https://restcountries.eu/rest/v2/region/${region}
+    const regionRequest = await fetch(`https://restcountries.eu/rest/v3/region/${region}
     `);
     return regionRequest.json();
   };
