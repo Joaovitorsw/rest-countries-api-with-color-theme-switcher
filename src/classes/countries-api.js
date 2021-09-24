409239a78e74e67a8d7abbfcf63dfc8f02e7e745
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-23 21:35:59 -0300

    📦 refactor: prefix url change

--- src/classes/countries-api.js
+++ src/classes/countries-api.js
@@ -1,12 +1,11 @@
 export class CountriesAPI {
   static getCountries = async () => {
-    const countries = await fetch(`https://restcountries.eu/rest/v3/all
-    `);
+    const countries = await fetch(`https://restcountries.com/v3/all`);
     return countries.json();
   };
 
   static getRegions = async (region) => {
-    const regionRequest = await fetch(`https://restcountries.eu/rest/v3/region/${region}
+    const regionRequest = await fetch(`https://restcountries.com/v3/region/${region}
     `);
     return regionRequest.json();
   };
