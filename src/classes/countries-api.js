a2380411158a726e211ffba766faedcfb5f45e0d
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-24 04:11:01 -0300

    ✨ feat: countries api cached countries

--- src/classes/countries-api.js
+++ src/classes/countries-api.js
@@ -1,12 +1,21 @@
+import { BasicStorage } from "./basic-storage.js";
+
 export class CountriesAPI {
   static getCountries = async () => {
+    const cachedCountries = BasicStorage.get("countries");
+    if (cachedCountries) return cachedCountries;
+
     const countries = await fetch(`https://restcountries.com/v3/all`);
-    return countries.json();
+    const data = await countries.json();
+    BasicStorage.set("countries", data);
+
+    return data;
   };
 
   static getRegions = async (region) => {
     const regionRequest = await fetch(`https://restcountries.com/v3/region/${region}
     `);
+
     return regionRequest.json();
   };
 }
