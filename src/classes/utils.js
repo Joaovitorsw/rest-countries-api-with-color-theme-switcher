33de372fb43fad8f6ef5f95fdf1b218f6d6a87a3
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-24 03:20:11 -0300

    ✨ feat: utils class

---
+++ src/classes/utils.js
@@ -0,0 +1,7 @@
+export class Utils {
+  static createElementWithClass(selector, ...className) {
+    const $element = document.createElement(selector);
+    $element.classList.add(...className);
+    return $element;
+  }
+}
