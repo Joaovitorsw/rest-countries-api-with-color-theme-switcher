3aee7a9d641b7536fc96e07d214f7ee1dfec7194
Joaovitorsw <joaovitorwbr@gmail.com>
2021-09-24 03:19:34 -0300

    📦 refactor: contries ui desctructuring object

--- src/classes/countries-ui.js
+++ src/classes/countries-ui.js
@@ -1,4 +1,5 @@
 import { CountriesAPI } from "../classes/countries-api.js";
+import { Utils } from "./utils.js";
 
 export class CountriesUI {
   $countriesContent;
@@ -44,31 +45,26 @@ export class CountriesUI {
     this.$countriesContent.innerHTML = "";
   }
 
-  createCountryCard(country) {
-    const $country = document.createElement("div");
-    $country.classList.add("country");
+  createCountryCard({ flags, region, cca2, cca3, capital, area, name } = country) {
+    const $country = Utils.createElementWithClass("div", "country");
+    const { common } = name;
 
-    $country.id = country.cca2;
-
-    const populationToDecimal = country.area.toLocaleString("pt-BR");
-
-    const countryInnerHTML = ` <a href="/#countrypage/${$country.id}"><img
-            src="${country.flags[1]}"
-            alt="${country.cca3}"
-            class="country-flag"
-          />
-          <h1 class="country-name">${country.name.official}</h1>
-          <h2 class="country-population">
-            Population:<span class="population-value">${populationToDecimal}</span>
-          </h2>
-          <h3 class="country-region">
-            Region:<span class="region-value">${country.region}</span>
-          </h3>
-          <h4 class="country-capital">
-            Capital:<span class="capital-value">${country.capital}</span>
-          </h4><a/>`;
+    const countryInnerHTML = ` 
+    <a href="/#countrypage/${cca2}">
+    <img src="${flags[1]}" alt="${cca3}" class="country-flag"/>
+    <h1 class="country-name">${common}</h1>
+    <h2 class="country-population"> Population:
+      <span class="population-value">${area.toLocaleString("pt-BR")}</span>
+    </h2>
+    <h3 class="country-region">
+     Region:<span class="region-value">${region}</span>
+     </h3>
+    <h4 class="country-capital">
+      Capital:<span class="capital-value">${capital}</span>
+     </h4><a/>`;
 
     $country.innerHTML = countryInnerHTML;
+
     this.#countriesClass();
     this.$countriesContent.appendChild($country);
   }
